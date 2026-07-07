e1b4cc455a28ac1cfbc08ec76a49740cef99c01e
Mark Speer <mark.speer11@gmail.com>
2026-07-07 07:11:49 -0400

    update transport

--- Source/UI/Transport/TransportBar.h
+++ Source/UI/Transport/TransportBar.h
@@ -1,77 +1,231 @@
-#pragma once
-
-#include "Engine/TransportController.h"
-
-namespace skeletonhive
-{
-
-class SessionManager;
-class SessionArrangementBridge;
-
-class TransportBar : public juce::Component,
-                     private juce::ChangeListener,
-                     private juce::Timer
-{
-public:
-    TransportBar (te::Edit& edit, TransportController& transport);
-    ~TransportBar() override;
-
-    std::function<void()> onAddAudioTrack;
-    std::function<void()> onAddMidiTrack;
-    std::function<void()> onAddMidiClip;
-    std::function<void()> onToggleMixer;
-    std::function<void()> onToggleSidechain;
-    std::function<void()> onToggleMidiLearn;
-
-    void setLearnModeActive (bool active);
-
-    void setSessionManager (SessionManager* manager);
-    void setSessionArrangementBridge (SessionArrangementBridge* bridge);
-    void setSessionViewActive (bool active);
-
-    std::function<void()> onToggleRecordToArrangement;
-    std::function<void()> onCaptureSession;
-    std::function<void()> onTogglePerformancePanel;
-
-    void setPerformancePanelVisible (bool visible);
-
-private:
-    void syncSessionControls();
-    void syncSessionCaptureControls();
-    void updateSessionControlsVisibility();
-    void resized() override;
-    void timerCallback() override;
-    void changeListenerCallback (juce::ChangeBroadcaster*) override;
-    void updateButtonStates();
-    void syncTempoControls();
-
-    te::Edit& edit;
-    TransportController& transportController;
-    SessionManager* sessionManager = nullptr;
-    SessionArrangementBridge* sessionArrangementBridge = nullptr;
-    bool sessionViewActive = false;
-
-    juce::Label viewLabel { {}, "Arrangement" };
-    juce::ComboBox launchQuantizeBox;
-    juce::ComboBox sceneLaunchModeBox;
-    juce::TextButton recordToArrangementButton { "Rec>" };
-    juce::TextButton captureButton { "Capture" };
-    juce::TextButton performanceButton { "Perf" };
-    juce::Label writePositionLabel { {}, "Write: 0.0.0" };
-
-    juce::TextButton returnButton { "<<" }, playButton { "Play" }, stopButton { "Stop" },
-        recordButton { "Rec" }, loopButton { "Loop" }, punchButton { "Punch" },
-        clickButton { "Click" }, takesButton { "Takes" };
-    juce::TextButton addAudioButton { "+ Audio" }, addMidiButton { "+ MIDI" },
-        addClipButton { "+ Clip" },
-        mixerButton { "Mixer" }, sidechainButton { "Sidechain" },
-        learnButton { "Learn" };
-    juce::Label positionLabel { {}, "00:00:00.000" };
-    juce::Slider tempoSlider;
-    juce::Label tempoLabel { {}, "BPM" };
-    juce::ComboBox timeSigBox;
-    juce::Slider clickVolumeSlider;
-    juce::ComboBox countInBox;
-};
-
-} // namespace skeletonhive
+#pragma once
+
+
+
+
+
+#include "Engine/TransportController.h"
+
+
+
+
+
+namespace skeletonhive
+
+
+{
+
+
+
+
+
+class SessionManager;
+
+
+class SessionArrangementBridge;
+
+
+
+
+
+class TransportBar : public juce::Component,
+
+
+                     private juce::ChangeListener,
+
+
+                     private juce::Timer
+
+
+{
+
+
+public:
+
+
+    TransportBar (te::Edit& edit, TransportController& transport);
+
+
+    ~TransportBar() override;
+
+
+
+
+
+    std::function<void()> onAddAudioTrack;
+
+
+    std::function<void()> onAddMidiTrack;
+
+
+    std::function<void()> onAddMidiClip;
+
+
+    std::function<void()> onToggleMixer;
+
+
+    std::function<void()> onToggleSidechain;
+
+
+    std::function<void()> onToggleMidiLearn;
+
+
+
+
+
+    void setLearnModeActive (bool active);
+
+
+
+
+
+    void setSessionManager (SessionManager* manager);
+
+
+    void setSessionArrangementBridge (SessionArrangementBridge* bridge);
+
+
+    void setSessionViewActive (bool active);
+
+
+
+
+
+    std::function<void()> onToggleRecordToArrangement;
+
+
+    std::function<void()> onCaptureSession;
+
+
+    std::function<void()> onTogglePerformancePanel;
+
+
+
+
+
+    void setPerformancePanelVisible (bool visible);
+
+
+
+
+
+private:
+
+
+    void syncSessionControls();
+
+
+    void syncSessionCaptureControls();
+
+
+    void updateSessionControlsVisibility();
+
+
+    void resized() override;
+
+
+    void timerCallback() override;
+
+
+    void changeListenerCallback (juce::ChangeBroadcaster*) override;
+
+
+    void updateButtonStates();
+
+
+    void syncTempoControls();
+
+
+
+
+
+    te::Edit& edit;
+
+
+    TransportController& transportController;
+
+
+    SessionManager* sessionManager = nullptr;
+
+
+    SessionArrangementBridge* sessionArrangementBridge = nullptr;
+
+
+    bool sessionViewActive = false;
+
+
+
+
+
+    juce::Label viewLabel { {}, "Arrangement" };
+
+
+    juce::ComboBox launchQuantizeBox;
+
+
+    juce::ComboBox sceneLaunchModeBox;
+
+
+    juce::TextButton recordToArrangementButton { "Rec>" };
+
+
+    juce::TextButton captureButton { "Capture" };
+
+
+    juce::TextButton performanceButton { "Perf" };
+
+
+    juce::Label writePositionLabel { {}, "Write: 0.0.0" };
+
+
+
+
+
+    juce::TextButton returnButton { "<<" }, playButton { "Play" }, stopButton { "Stop" },
+
+
+        recordButton { "Rec" }, loopButton { "Loop" }, punchButton { "Punch" },
+
+
+        clickButton { "Click" }, takesButton { "Takes" };
+
+
+    juce::TextButton addAudioButton { "+ Audio" }, addMidiButton { "+ MIDI" },
+
+
+        addClipButton { "+ Clip" },
+
+
+        mixerButton { "Mixer" }, sidechainButton { "Sidechain" },
+
+
+        learnButton { "Learn" };
+
+
+    juce::Label positionLabel { {}, "00:00:00.000" };
+
+
+    juce::Slider tempoSlider;
+
+
+    juce::Label tempoLabel { {}, "BPM" };
+
+
+    juce::ComboBox timeSigBox;
+
+
+    juce::Slider clickVolumeSlider;
+
+
+    juce::ComboBox countInBox;
+
+
+};
+
+
+
+
+
+} // namespace skeletonhive
+
+
